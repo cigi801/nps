d1bf97efe9801d4c8371746650473d8f708ab67f
Cigi <54045350+cigi801@users.noreply.github.com>
2025-05-25 00:28:00 -0600

    Week 5 - Added Current Conditions Page

--- src/js/templates.mjs
+++ src/js/templates.mjs
@@ -40,5 +40,36 @@ export function parkInfoTemplate(info) {
     </section>`;
   }
 
+export function alertTemplate(alert) {
+  let alertType = "";
+  // most of the alerts are one word and line up with the icons nicely. "Park Closure" is the exception
+  switch (alert.category) {
+    case "Park Closure":
+      alertType = "closure";
+      break;
+    default:
+      alertType = alert.category?.toLowerCase() || "general";
+  }
+  return `<li class="alert">
+  <svg class="icon" focusable="false" aria-hidden="true">
+    <use xlink:href="/images/sprite.symbol.svg#alert-${alertType}"></use>
+  </svg>
+  <div>
+    <h3 class="alert-${alertType}">${alert.title}</h3>
+    <p>${alert.description}</p>
+  </div></li>`;
+}
+
+export function visitorCenterTemplate(center) {
+  return `<li class="visitor-center">
+  <h4>${center.name}</h4>
+  <p>${center.description}</p>
+  <p>${center.directionsInfo}</p>
+  </li>`;
+}
+
+export function activityListTemplate(activities) {
+  return activities.map((activity) => `<li>${activity.name}</li>`).join("");
+}
  
 
